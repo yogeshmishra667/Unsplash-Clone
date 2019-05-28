33827d4bfae2d2ddce6a7f467883d7203f5a5ec3
YoGi <yogeshmishra667@gmail.com>
2019-05-28 12:02:37 +0530

    i'm the one

---
+++ precache-manifest.189ac889d1b2ad656de4e11251f27202.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965d4b6827d58a8360b9e8cefcc8d0e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f63bd436fd2d5ea4afb1",
+    "url": "/static/css/main.0564f1f4.chunk.css"
+  },
+  {
+    "revision": "fb5ebb17ebde065bf9c9",
+    "url": "/static/js/2.396ad7fd.chunk.js"
+  },
+  {
+    "revision": "f63bd436fd2d5ea4afb1",
+    "url": "/static/js/main.5ba85467.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
